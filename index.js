8f18a7a84f1e108966818ca1164f962b546c4fbd
tuna <tsune10696@gmail.com>
2024-04-21 20:42:14 +0900

    240421-9

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ import { firestore } from "./firebase.js";
 
 window.onload = async () => {
 
-////データの読み取り(1件)
-const docId = "iha1dt84ul3r";
-const doc = await firestore.collection("projectManagement").doc(docId).get();
-console.log(doc);
-console.log(doc.id);
-console.log(doc.data());
+    ////データの読み取り(1件)
+    const docId = "iha1dt84ul3r";
+    const doc = await firestore.collection("projectManagement").doc(docId).get();
+    
+    if (doc.exists) {
+        console.log(doc.id);
+        console.log(doc.data());
+    } else {
+        console.log("Document does not exist!");
+    }
 };
+
 //データの読み取り(複数)
 // const snapShot = await firestore.collection("advertisementConsultation").get();
 // const proj = snapShot.docs.map((doc) => ({
