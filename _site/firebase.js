f23d2c218bbf99fde5715f240a24d03db910606c
tuna <tsune10696@gmail.com>
2024-04-21 18:31:47 +0900

    240421-2

---
+++ _site/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBP_enJuXumqo5SoXHxYi1uS7n3KaYwGo8",
+    authDomain: "poimaruapp.firebaseapp.com",
+    databaseURL: "https://poimaruapp-default-rtdb.firebaseio.com",
+    projectId: "poimaruapp",
+    storageBucket: "poimaruapp.appspot.com",
+    messagingSenderId: "409133139197",
+    appId: "1:409133139197:web:f8f5440bb44c6bf0ebaa94",
+    measurementId: "G-PKJ70BRLFS"
+  };
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
---
+++ _site/index.js
@@ -0,0 +1,11 @@
+import { firestore } from "./firebase.js";
+
+window.onload = async () => {
+
+const docId = "iha1dt84ul3r";
+const docRef = firestore.collection("projectManagement").doc(docId);
+const doc = await docRef.get();
+console.log(doc);
+console.log(doc.id);
+console.log(doc.tada());
+};
