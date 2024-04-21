cd688067002c438d0e7f172a20b3b8363e14afe1
tuna <tsune10696@gmail.com>
2024-04-21 18:44:20 +0900

    240421-3

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import { firestore } from "./firebase.js";
 window.onload = async () => {
 
 const docId = "iha1dt84ul3r";
-const docRef = firestore.collection("projectManagement").doc(docId);
-const doc = await docRef.get();
+const doc = await firestore.collection("projectManagement").doc(docId).get();
 console.log(doc);
 console.log(doc.id);
 console.log(doc.data());
